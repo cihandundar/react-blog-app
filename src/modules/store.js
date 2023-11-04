5cd3d9f5f6bb2baffcab7b89203d81e8901cc48e
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-11-05 01:19:22 +0300

    fix

---
+++ src/modules/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import userReducer from "../redux/userSlice";
+export const store = configureStore({
+  reducer: {
+    users: userReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(logger),
+});
