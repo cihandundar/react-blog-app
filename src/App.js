478c12a71df00cc5757dbf93577e708f5a7064e4
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-11-04 22:56:42 +0300

    router structure created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Aside } from "./components";
+import { Posts, Users } from "./pages";
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <main>
+        <Aside />
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/post" element={<Posts />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
