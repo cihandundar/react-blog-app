573532910a2cd42aa529f31bcd11a485f4422cbb
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-11-16 22:21:33 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./styles/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>
+  </React.Fragment>
 );
