3d6a44a8f61cb453f58a4f6744e4604991b99682
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-11-17 16:00:48 +0300

    hero props'a çevrildi

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,22 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa";
-const Hero = () => {
+
+const Hero = ({ title, description, linkText, linkUrl }) => {
   return (
     <div className="hero">
       <div className="container">
-        <div className="hero__title">Welcome to Our Blog</div>
-        <div className="hero__desc">
-          In this blog, you'll find inspiring stories, tips, and fresh insights
-          that touch every corner of life. Our goal is to provide content that
-          not only entertains but also broadens your perspective. Whether you're
-          sipping your coffee and diving deep or just skimming through
-          intriguing articles, there's something here for everyone. Let's start
-          exploring!
-        </div>
-        <Link to="/">
-          Learn More <FaArrowRight />
-        </Link>
+        <div className="hero__title">{title}</div>
+        {description && <div className="hero__desc">{description}</div>}
+        {linkText && linkUrl && (
+          <Link to={linkUrl} className="hero__link">
+            {linkText}
+            <FaArrowRight />
+          </Link>
+        )}
       </div>
     </div>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,17 @@ import React from "react";
 const Home = () => {
   return (
     <section>
-      <Hero />
+      <Hero
+        title="Welcome to Our Blog"
+        description="In this blog, you'll find inspiring stories, tips, and fresh insights
+          that touch every corner of life. Our goal is to provide content that
+          not only entertains but also broadens your perspective. Whether you're
+          sipping your coffee and diving deep or just skimming through
+          intriguing articles, there's something here for everyone. Let's start
+          exploring!"
+        linkText="Learn More"
+        linkUrl="/"
+      />
       <div className="container">
         <div className="row">
           <BlogItem />
